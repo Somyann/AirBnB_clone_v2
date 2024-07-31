b7a612829d9846ff66ec11b4a0b350ce03676903
Somyann <somiaprecious91@gmail.com>
2024-07-31 17:50:42 +0000

    new

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -11,37 +11,44 @@ Routes:
     /number_template/<n>: Displays an HTML page only if <n> is an integer.
 """
 from flask import Flask
-from flask import render_template
+from flask import abort
 
 app = Flask(__name__)
+
+"""u entered the URL manually"""
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
+
+""" was not found on the server"""
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
+    """Displays 'HBNB'."""
     return "HBNB"
+
+""" was not found on the server"""
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>
+    """Displays 'C' followed by the value of <text>.
 
     Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
-
+"""not found on the server."""
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
+
 def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>
+    """Displays 'Python' followed by the value of <text>.
 
     Replaces any underscores in <text> with slashes.
     """
@@ -51,15 +58,14 @@ def python(text="is cool"):
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if <n> is an integer."""
+    """Displays 'n is a number' only if n is an integer."""
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number_template(n):
-    """Displays an HTML page only if <n> is an integer."""
-    return render_template("5-number.html", n=n)
-
+"""Displays an HTML page only if <n> is an integer."""
+     return render_templates("5-number.html",number=n)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
